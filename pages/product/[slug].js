eb1ec09da45a922d865d438213c9a11dd1cb1ad7
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-09-20 19:19:04 +0300

    f

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -21,13 +21,14 @@ const ProductDetails = ({ product, products }) => {
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[index])} className="product-detail-image" />
+            <img src={urlFor(image && image[index])} alt={name} className="product-detail-image" />
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
               <img 
                 key={i}
                 src={urlFor(item)}
+                alt={item.name}
                 className={i === index ? 'small-image selected-image' : 'small-image'}
                 onMouseEnter={() => setIndex(i)}
               />
