89664a9db3cce6ce84edc971d0d5b1e0542d14aa
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-09-27 13:12:03 +0300

    3

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -4,17 +4,18 @@ import { urlFor } from '../lib/client';
 
 const HeroBanner = ({ heroBanner }) => {
   return (
-    <div className="hero-banner-container">
-      <div className="hero-banner-left-container">
-        <p className="beats-solo">{heroBanner.smallText}</p>
+    <div className="hero-banner__container">
+      <div className="hero-banner__left-container">
+        <p className="hero-banner__smallText">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
-        <h1 className="hero-banner-image-h1">{heroBanner.largeText}</h1>
+        <h1>{heroBanner.largeText}</h1>
         <Link href={`/product/${heroBanner.product}`}>
           <button type="button">{heroBanner.buttonText}</button>
         </Link>
       </div>
-      <div className="hero-banner-image-container">
-        <img src={urlFor(heroBanner.image)} alt={`${heroBanner.product}`} className="hero-banner-image" />
+      <div className="hero-banner__image-container">
+        <img src={urlFor(heroBanner.image)} alt={`${heroBanner.product}`} 
+        className="hero-banner__image" />
       </div>
     </div>
   )
