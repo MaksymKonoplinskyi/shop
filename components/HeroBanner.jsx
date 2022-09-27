9feb97eeee2d23b044578c11ba7a701cd7921ffc
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-09-27 12:05:11 +0300

    2

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/client';
-import styles from './HeroBanner.module.css';
 
 const HeroBanner = ({ heroBanner }) => {
   return (
